30fe0404e92cd6940b2c236bdce5e23b2f3e4ffd
Petri Silen <petri.silen@nokia.com>
2023-01-16 14:20:13 +0200

    Changes

--- src/app/pages/alerts/view/AlertsPageView.tsx
+++ src/app/pages/alerts/view/AlertsPageView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TriggersPageView from '../../../common/components/triggerspage/view/TriggersPageView';
 
-export default function AlertsPageView() {
-  return <TriggersPageView pageStateNamespace="alertsPage" />;
-}
+const AlertsPageView = () => <TriggersPageView pageStateNamespace="alertsPage" />;
+
+export default AlertsPageView;
