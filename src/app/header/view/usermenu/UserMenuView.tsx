514e1ef75f4bde13d36d1e42d1219782c9345047
pksilen <petri.silen@silensoft.com>
2023-02-28 15:15:59 +0200

    Changes

--- src/app/header/view/usermenu/UserMenuView.tsx
+++ src/app/header/view/usermenu/UserMenuView.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Icon } from 'semantic-ui-react';
 import styles from './UserMenuView.module.scss';
-import styles2 from '../HeaderView.module.scss';
 
 const UserMenuView = () => (
   <section className={styles.userMenu}>
-    <span className={`${styles2.headerIcon} ${styles.userIcon}`} />
+    <Icon name="user" />
     <span>Petri</span>
     <Icon name="dropdown" />
   </section>
