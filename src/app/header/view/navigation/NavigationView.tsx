f43b53410acf29bc8bd60c551c7acd50c67fa505
pksilen <petri.silen@silensoft.com>
2023-03-02 18:38:12 +0200

    Changes

--- src/app/header/view/navigation/NavigationView.tsx
+++ src/app/header/view/navigation/NavigationView.tsx
@@ -14,45 +14,23 @@ const NavigationView = ({ selectPage }: Props) => {
   return (
     <nav className={styles.navigation}>
       <Link className={styles.link} to="/dashboards" onClick={() => selectPage('dashboardsPage')}>
-        <Icon
-          className={`${styles.linkIcon} ${pathname === '/dashboards' ? styles.selectedIcon : ''}`}
-          name="dashboard"
-          color="teal"
-        />
-        <span className={`${styles.linkText} ${pathname === '/' || pathname === '/dashboards' ? styles.selected : ''}`}>
-          DASHBOARDS
-        </span>
+        <Icon className={styles.linkIcon} name="dashboard" color="teal" />
+        <span className={`${styles.linkText} ${pathname === '/dashboards' ? styles.selected : ''}`}>DASHBOARDS</span>
       </Link>
       <Link className={styles.link} to="/reports" onClick={() => selectPage('reportsPage')}>
-        <Icon
-          className={`${styles.linkIcon} ${pathname === '/reports' ? styles.selectedIcon : ''}`}
-          name="file"
-          color="teal"
-        />
+        <Icon className={styles.linkIcon} name="file" color="teal" />
         <span className={`${styles.linkText} ${pathname === '/reports' ? styles.selected : ''}`}>REPORTS</span>
       </Link>
       <Link className={styles.link} to="/alerts" onClick={() => selectPage('alertsPage')}>
-        <Icon
-          className={`${styles.linkIcon} ${pathname === '/alerts' ? styles.selectedIcon : ''}`}
-          name="alarm"
-          color="teal"
-        />
+        <Icon className={styles.linkIcon} name="alarm" color="teal" />
         <span className={`${styles.linkText} ${pathname === '/alerts' ? styles.selected : ''}`}>ALERTS</span>
       </Link>
       <Link className={styles.link} to="/goals" onClick={() => selectPage('goalsPage')}>
-        <Icon
-          className={`${styles.linkIcon} ${pathname === '/goals' ? styles.selectedIcon : ''}`}
-          name="target"
-          color="teal"
-        />
-        <span className={`${styles.linkText} ${pathname === '/goals' ? styles.selected : ''}`}>GOALS</span>
+        <Icon className={styles.linkIcon} name="target" color="teal" />
+        <span className={styles.linkText}>GOALS</span>
       </Link>
       <Link className={styles.link} to="/data-explorer" onClick={() => selectPage('dataExplorerPage')}>
-        <Icon
-          className={`${styles.linkIcon} ${pathname === '/data-explorer' ? styles.selectedIcon : ''}`}
-          name="database"
-          color="teal"
-        />
+        <Icon className={styles.linkIcon} name="database" color="teal" />
         <span className={`${styles.linkText} ${pathname === '/data-explorer' ? styles.selected : ''}`}>
           DATA EXPLORER
         </span>
