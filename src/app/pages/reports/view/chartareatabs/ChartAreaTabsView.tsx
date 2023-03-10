83e3b8c1fc16991ec8df8caf6a4a2c833b312a69
Petri Silen <petri.silen@nokia.com>
2023-03-10 14:26:18 +0200

    Changes

--- src/app/pages/reports/view/chartareatabs/ChartAreaTabsView.tsx
+++ src/app/pages/reports/view/chartareatabs/ChartAreaTabsView.tsx
@@ -1,29 +1,50 @@
-import React from 'react';
-import { Tab } from 'semantic-ui-react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from 'react';
+import { Icon, Tab } from 'semantic-ui-react';
 import ChartAreaView from '../../../../common/components/chartarea/view/ChartAreaView';
 import styles from './ChartAreaTabsView.module.scss';
 
 const ChartAreaTabsView = () => {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
   const panes = [
     {
-      menuItem: 'Subs 0504877334 Failures',
+      menuItem: (
+        <span
+          className={`item ${activeTabIndex === 0 ? 'active' : ''}`}
+          key="Subs 0504877334 Failures"
+          onClick={() => setActiveTabIndex(0)}
+        >
+          Subs 0504877334 Failures
+          <Icon className={styles.closeIcon} name="close" />
+        </span>
+      ),
       render: () => (
         <Tab.Pane>
-          <ChartAreaView className={styles.report} pageStateNamespace="reportsPage" />
+          <ChartAreaView className={styles.chartArea} pageStateNamespace="reportsPage" />
         </Tab.Pane>
       )
     },
     {
-      menuItem: 'Subs 0504877334 Throughput',
+      menuItem: (
+        <span
+          className={`item ${activeTabIndex === 1 ? 'active' : ''}`}
+          key="Subs 0504877334 Throughput"
+          onClick={() => setActiveTabIndex(1)}
+        >
+          Subs 0504877334 Throughput
+          <Icon className={styles.closeIcon} name="close" />
+        </span>
+      ),
       render: () => (
         <Tab.Pane>
-          <ChartAreaView className={styles.report} pageStateNamespace="reportsPage" />
+          <ChartAreaView className={styles.chartArea} pageStateNamespace="reportsPage" />
         </Tab.Pane>
       )
     }
   ];
 
-  return <Tab className={styles.tab} activeIndex={0} panes={panes} />;
+  return <Tab activeIndex={activeTabIndex} className={styles.tab} panes={panes} />;
 };
 
 export default ChartAreaTabsView;
