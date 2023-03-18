e3c1b16cac27caf53f8a01dff3905aed4c978ab9
pksilen <petri.silen@silensoft.com>
2023-03-18 16:55:02 +0200

    Changes

--- src/app/common/components/triggerspage/view/detailsdialog/TriggerDetailsDialogView.tsx
+++ src/app/common/components/triggerspage/view/detailsdialog/TriggerDetailsDialogView.tsx
@@ -23,61 +23,61 @@ const TriggerDetailsDialogView = ({ closeDialog }: Props) => (
       <Form>
         <Form.Field>
           <Form.Group>
-            <label>Severity</label>
+            <label className={styles.label}>Severity</label>
             <input className={styles.critical} value="Critical" />
           </Form.Group>
         </Form.Field>
         <Form.Field>
           <Form.Group>
-            <label>Description</label>
+            <label className={styles.label}>Description</label>
             <input className="borderless large" value="Very High gNB Setup Failure Rate" />
           </Form.Group>
         </Form.Field>
         <Form.Field>
           <Form.Group>
-            <label>Data source</label>
+            <label className={styles.label}>Data source</label>
             <input className="borderless" value="Active CNI Alerts" />
           </Form.Group>
         </Form.Field>
         <Form.Field>
           <Form.Group>
-            <label>Trigger time</label>
+            <label className={styles.label}>Trigger time</label>
             <input className="borderless" value="2019-04-01 12:00:00" />
           </Form.Group>
         </Form.Field>
         <Form.Field>
           <Form.Group>
-            <label>Active duration</label>
+            <label className={styles.label}>Active duration</label>
             <input className="borderless" value="3 hours" />
           </Form.Group>
         </Form.Field>
         <Form.Field>
           <Form.Group>
-            <label>Labels</label>
+            <label className={styles.label}>Labels</label>
             <input className="borderless" value="5G RAN, gNB 2345, Error: Signalling Congestion" />
           </Form.Group>
         </Form.Field>
         <Form.Field>
           <Form.Group>
-            <label>Trigger values</label>
+            <label className={styles.label}>Trigger values</label>
             <input className="borderless" value="Setup Failure Rate: 18%" />
           </Form.Group>
         </Form.Field>
         <Form.Field>
           <Form.Group>
-            <label>Status</label>
+            <label className={styles.label}>Status</label>
             <input className="borderless" value="New" />
           </Form.Group>
         </Form.Field>
         <Form.Field>
           <Form.Group>
-            <label>Assignee</label>
+            <label className={styles.label}>Assignee</label>
             <input className="borderless" value="Petri Silen" />
           </Form.Group>
         </Form.Field>
         <Form.Field>
           <Form.Group>
-            <label>Status last modified</label>
+            <label className={styles.label}>Status last modified</label>
             <input className="borderless" value="2019-04-01 12:00:00" />
           </Form.Group>
         </Form.Field>
