12bb5db53678bf524959d74013e85161e7da0ff3
pksilen <petri.silen@silensoft.com>
2023-03-18 16:52:28 +0200

    Changes

--- src/app/common/components/triggerspage/view/detailsdialog/TriggerDetailsDialogView.tsx
+++ src/app/common/components/triggerspage/view/detailsdialog/TriggerDetailsDialogView.tsx
@@ -9,26 +9,78 @@ type Props = {
 const TriggerDetailsDialogView = ({ closeDialog }: Props) => (
   <Modal
     className={styles.modal}
-    closeOnEscape
+    closeOnEscapes
     closeOnDimmerClick
     onClose={closeDialog}
     onKeyDown={stopEventPropagation}
     open
   >
-    <Modal.Header className={styles.header}>
+    <Modal.Header className={`${styles.header} withBackButton`}>
       <Icon className={styles.icon} name="angle left" onClick={closeDialog} size="big" />
       <div className={styles.text}> ALERT DETAILS</div>
     </Modal.Header>
     <Modal.Content>
       <Form>
-        <Form.Group>
-          <label>Severity</label>
-          <input readOnly value="Critical" />
-        </Form.Group>
-        <Form.Group>
-          <label>Description</label>
-          <input readOnly value="Description" />
-        </Form.Group>
+        <Form.Field>
+          <Form.Group>
+            <label>Severity</label>
+            <input className={styles.critical} value="Critical" />
+          </Form.Group>
+        </Form.Field>
+        <Form.Field>
+          <Form.Group>
+            <label>Description</label>
+            <input className="borderless large" value="Very High gNB Setup Failure Rate" />
+          </Form.Group>
+        </Form.Field>
+        <Form.Field>
+          <Form.Group>
+            <label>Data source</label>
+            <input className="borderless" value="Active CNI Alerts" />
+          </Form.Group>
+        </Form.Field>
+        <Form.Field>
+          <Form.Group>
+            <label>Trigger time</label>
+            <input className="borderless" value="2019-04-01 12:00:00" />
+          </Form.Group>
+        </Form.Field>
+        <Form.Field>
+          <Form.Group>
+            <label>Active duration</label>
+            <input className="borderless" value="3 hours" />
+          </Form.Group>
+        </Form.Field>
+        <Form.Field>
+          <Form.Group>
+            <label>Labels</label>
+            <input className="borderless" value="5G RAN, gNB 2345, Error: Signalling Congestion" />
+          </Form.Group>
+        </Form.Field>
+        <Form.Field>
+          <Form.Group>
+            <label>Trigger values</label>
+            <input className="borderless" value="Setup Failure Rate: 18%" />
+          </Form.Group>
+        </Form.Field>
+        <Form.Field>
+          <Form.Group>
+            <label>Status</label>
+            <input className="borderless" value="New" />
+          </Form.Group>
+        </Form.Field>
+        <Form.Field>
+          <Form.Group>
+            <label>Assignee</label>
+            <input className="borderless" value="Petri Silen" />
+          </Form.Group>
+        </Form.Field>
+        <Form.Field>
+          <Form.Group>
+            <label>Status last modified</label>
+            <input className="borderless" value="2019-04-01 12:00:00" />
+          </Form.Group>
+        </Form.Field>
       </Form>
     </Modal.Content>
   </Modal>
