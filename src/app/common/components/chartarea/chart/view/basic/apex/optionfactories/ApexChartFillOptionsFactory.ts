8bcab345478ce4cb35cbd5e6095ae935a1152d9f
pksilen <petri.silen@silensoft.com>
2023-03-19 11:33:38 +0200

    Changes

--- src/app/common/components/chartarea/chart/view/basic/apex/optionfactories/ApexChartFillOptionsFactory.ts
+++ src/app/common/components/chartarea/chart/view/basic/apex/optionfactories/ApexChartFillOptionsFactory.ts
@@ -2,7 +2,7 @@ import type { Chart } from '../../../../model/state/Chart';
 
 export default class ApexChartFillOptionsFactory {
   static createFillOptions(chart: Chart): object {
-    const gradientFillType = chart.getGradientFillType;
+    const gradientFillType = chart.getGradientFillType();
     const opacity = chart.getFillOpacity();
 
     const gradientFill = {
