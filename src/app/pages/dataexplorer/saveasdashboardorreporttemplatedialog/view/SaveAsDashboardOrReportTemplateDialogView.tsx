aaf0a18317a40f3360f18141f7be6f51f478b152
pksilen <petri.silen@silensoft.com>
2023-03-22 10:44:09 +0200

    Changes

--- src/app/pages/dataexplorer/saveasdashboardorreporttemplatedialog/view/SaveAsDashboardOrReportTemplateDialogView.tsx
+++ src/app/pages/dataexplorer/saveasdashboardorreporttemplatedialog/view/SaveAsDashboardOrReportTemplateDialogView.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { connect } from 'react-redux';
-import { Button, Dropdown, DropdownProps, Form, Icon, Input, Message, Modal, Radio } from 'semantic-ui-react';
+import { Button, Dropdown, DropdownProps, Form, Icon, Input, Message, Modal } from 'semantic-ui-react';
 import styles from './SaveAsDashboardOrReportTemplateDialogView.module.scss';
 import type { DashboardGroup } from '../../../dashboards/model/state/types/DashboardGroup';
 import { ActionDispatchers, controller, State } from '../controller/saveAsDashboardOrReportTemplateDialogController';
@@ -50,31 +50,36 @@ const SaveAsDashboardOrReportTemplateDialogView = ({
   return (
     <>
       <Modal closeOnEscape onClose={closeDialog} open={isOpen}>
-        <Modal.Header content="SAVE AS DASHBOARD" />
+        <Modal.Header content="SAVE AS DASHBOARD OR REPORT TEMPLATE" />
         <Modal.Content>
           <Form>
             <Form.Field>
-              <Radio label="Save as new dashboard" name="radioGroup" defaultChecked />
+              <Form.Group>
+                <label>Save as</label>
+                <Form.Radio className={styles.noLeftPadding} label="Dashboard" name="radioGroup" defaultChecked />
+                <Form.Radio label="Report template" name="radioGroup" />
+              </Form.Group>
             </Form.Field>
-            <Form.Field>
-              <Radio disabled label="Save as new report template" name="radioGroup" value="that" />
+            <Form.Field className={styles.formField}>
+              <Form.Group>
+                <label>Dashboard group</label>
+                <Dropdown
+                  placeholder="Type to add new dashboard group"
+                  search
+                  selection
+                  additionLabel="Add dashboard group: "
+                  additionPosition="top"
+                  allowAdditions
+                  options={dashboardGroupOptions}
+                  onChange={selectDashboardGroup}
+                />
+              </Form.Group>
             </Form.Field>
-            <Form.Field className={styles.dashboardGroupField}>
-              <label>Dashboard group</label>
-              <Dropdown
-                placeholder="Type to add new dashboard group"
-                search
-                selection
-                additionLabel="Add dashboard group: "
-                additionPosition="top"
-                allowAdditions
-                options={dashboardGroupOptions}
-                onChange={selectDashboardGroup}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>Dashboard name</label>
-              <Input onChange={changeDashboardName} value={dashboardName} />
+            <Form.Field className={styles.formField}>
+              <Form.Group>
+                <label>Dashboard name</label>
+                <Input onChange={changeDashboardName} value={dashboardName} />
+              </Form.Group>
             </Form.Field>
           </Form>
         </Modal.Content>
@@ -84,6 +89,7 @@ const SaveAsDashboardOrReportTemplateDialogView = ({
           </Button>
           <Button
             primary
+            positive
             onClick={() =>
               saveDashboard(dashboardGroupName, dashboardName, dashboardGroups, {
                 charts,
@@ -92,7 +98,7 @@ const SaveAsDashboardOrReportTemplateDialogView = ({
               })
             }
           >
-            SAVE
+            OK
           </Button>
         </Modal.Actions>
       </Modal>
