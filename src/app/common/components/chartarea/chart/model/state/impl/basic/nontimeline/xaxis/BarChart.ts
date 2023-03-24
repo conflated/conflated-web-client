acb4d110702fb21e6a5a296b009c99911a373cff
Petri Silen <petri.silen@nokia.com>
2023-03-24 11:57:32 +0200

    Changes

--- src/app/common/components/chartarea/chart/model/state/impl/basic/nontimeline/xaxis/BarChart.ts
+++ src/app/common/components/chartarea/chart/model/state/impl/basic/nontimeline/xaxis/BarChart.ts
@@ -1,6 +1,11 @@
 import AbstractXAxisChart from './AbstractXAxisChart';
+import { MeasureVisualizationType } from '../../../../selectedmeasure/types/MeasureVisualizationType';
 
 export default class BarChart extends AbstractXAxisChart {
+  getNextMeasureVisualizationType(): MeasureVisualizationType {
+    return 'column';
+  }
+
   getSubtitleText(): string {
     if (this.selectedDimensions.length === 0 && this.selectedMeasures.length > 0) {
       return 'All';
