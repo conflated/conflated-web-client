87c52fc3dd3c8701bf1a7a614fe715546c7ee86c
pksilen <petri.silen@silensoft.com>
2023-03-30 11:15:24 +0300

    Changes

--- src/app/common/components/chartarea/chart/view/basic/optionfactories/ApexChartGridOptionsFactory.ts
+++ src/app/common/components/chartarea/chart/view/basic/optionfactories/ApexChartGridOptionsFactory.ts
@@ -6,7 +6,7 @@ export default class ApexChartGridOptionsFactory {
 
     return {
       show: chart.shouldShowGrid(),
-      borderColor: isDarkModeActive ? '#171717' : '#e9e9e9',
+      borderColor: isDarkModeActive ? '#1f1f1f' : '#e9e9e9',
       xaxis: {
         lines: {
           show: false
