28cea81d75eb4587f50563224d4aced0f6b8b12d
pksilen <petri.silen@silensoft.com>
2023-04-10 20:39:59 +0300

    Changes

--- src/app/common/views/input/searchinput/SearchInputView.tsx
+++ src/app/common/views/input/searchinput/SearchInputView.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useRef } from 'react';
-import { Input } from 'semantic-ui-react';
+import { Input, Popup } from 'semantic-ui-react';
 
 type Props = {
   className: string;
@@ -19,15 +19,22 @@ const SearchInputView = ({ className, isShown, onChange }: Props) => {
 
   if (isShown) {
     return (
-      <Input
-        className={className}
-        placeholder="Search..."
-        fluid
-        icon="search"
-        iconPosition="left"
-        size="small"
-        ref={inputRef}
-        onChange={({ currentTarget: { value } }: React.SyntheticEvent<HTMLInputElement>) => onChange(value)}
+      <Popup
+        content="Type keyword(s) to search, for a negated search, prefix the keyword(s) with a minus sign, e.g. -failure to find all measures not containing the word 'failure'"
+        inverted
+        mouseEnterDelay={2000}
+        trigger={
+          <Input
+            className={className}
+            placeholder="Search..."
+            fluid
+            icon="search"
+            iconPosition="left"
+            size="small"
+            ref={inputRef}
+            onChange={({ currentTarget: { value } }: React.SyntheticEvent<HTMLInputElement>) => onChange(value)}
+          />
+        }
       />
     );
   }
