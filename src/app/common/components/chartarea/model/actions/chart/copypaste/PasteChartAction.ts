3d37c930468473d56d3a7d1f3e5f18dd2d1022a1
pksilen <petri.silen@silensoft.com>
2023-04-10 10:57:50 +0300

    Changes

--- src/app/common/components/chartarea/model/actions/chart/copypaste/PasteChartAction.ts
+++ src/app/common/components/chartarea/model/actions/chart/copypaste/PasteChartAction.ts
@@ -2,6 +2,7 @@ import AbstractChartAreaAction from '../../AbstractChartAreaAction';
 import type { ChartAreaState } from '../../../state/ChartAreaState';
 import type { Chart } from '../../../../chart/model/state/Chart';
 import type { ChartAreaStateNamespace } from '../../../state/types/ChartAreaStateNamespace';
+import ChartFactory from '../../../../chart/model/state/ChartFactory';
 
 export default class PasteChartAction extends AbstractChartAreaAction {
   constructor(stateNamespace: ChartAreaStateNamespace, private readonly chart: Chart) {
@@ -15,11 +16,14 @@ export default class PasteChartAction extends AbstractChartAreaAction {
       return currentState;
     }
 
-    copiedChart.id = this.chart.id;
+    const pastedChart = ChartFactory.createChart(copiedChart.getChartConfiguration());
+    pastedChart.id = this.chart.id;
+    const newCharts = charts.map((chart) => (chart === this.chart ? pastedChart : chart));
 
     return {
       ...currentState,
-      charts: charts.map((chart) => (chart === this.chart ? copiedChart : chart))
+      charts: newCharts,
+      selectedChart: pastedChart
     };
   }
 }
