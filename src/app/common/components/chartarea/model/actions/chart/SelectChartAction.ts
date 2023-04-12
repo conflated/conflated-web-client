34440f337e674bf317ded52ec82fb3106e8209b7
Petri Silen <petri.silen@nokia.com>
2023-04-12 15:44:28 +0300

    Changes

--- src/app/common/components/chartarea/model/actions/chart/SelectChartAction.ts
+++ src/app/common/components/chartarea/model/actions/chart/SelectChartAction.ts
@@ -3,7 +3,7 @@ import type { ChartAreaState } from '../../state/ChartAreaState';
 import type { Chart } from '../../../chart/model/state/Chart';
 import { ChartAreaStateNamespace } from '../../state/types/ChartAreaStateNamespace';
 import emptyDataSource from '../../../chart/model/state/datasource/emptyDataSource';
-import ChartFactory from '../../../chart/model/state/ChartFactory';
+import { nullChart } from '../../state/createChartAreaStateReducer';
 
 export default class SelectChartAction extends AbstractChartAreaAction {
   constructor(stateNamespace: ChartAreaStateNamespace, private readonly chart: Chart) {
@@ -14,10 +14,14 @@ export default class SelectChartAction extends AbstractChartAreaAction {
     const { selectedChart } = currentState;
 
     if (selectedChart === this.chart) {
-      return {
-        ...currentState,
-        selectedChart: ChartFactory.createChart()
-      };
+      if (this.stateNamespace === 'dataExplorerPage') {
+        return currentState;
+      } else {
+        return {
+          ...currentState,
+          selectedChart: nullChart
+        };
+      }
     } else {
       if (this.chart.dataSource === emptyDataSource && selectedChart.dataSource !== emptyDataSource) {
         this.chart.dataSource = selectedChart.dataSource;
