d9406856cda5e8d9004022c64f8c2f3d78930cd2
Petri Silen <petri.silen@nokia.com>
2023-04-14 14:52:54 +0300

    Changes

--- src/app/common/components/chartarea/chart/view/confighints/ChartConfigHintsView.tsx
+++ src/app/common/components/chartarea/chart/view/confighints/ChartConfigHintsView.tsx
@@ -11,6 +11,60 @@ type Props = {
 const ChartConfigHintsView: React.FC<Props> = ({ chart, heightInRows, widthInCols }: Props) => {
   const chartConfigHintTitle = chart.getChartConfigHintTitle();
   const chartConfigHintSubtitle = chart.getChartConfigHintSubtitle();
+  let chartIcon;
+
+  switch (chart.chartType) {
+    case 'column':
+      chartIcon = styles.columnChart;
+      break;
+    case 'bar':
+      chartIcon = styles.barChart;
+      break;
+    case 'area':
+      chartIcon = styles.areaChart;
+      break;
+    case 'rangeArea':
+      chartIcon = styles.rangeAreaChart;
+      break;
+    case 'line':
+      chartIcon = styles.lineChart;
+      break;
+    case 'donut':
+      chartIcon = styles.donutChart;
+      break;
+    case 'pie':
+      chartIcon = styles.pieChart;
+      break;
+    case 'bubble':
+      chartIcon = styles.bubbleChart;
+      break;
+    case 'scatter':
+      chartIcon = styles.scatterPlot;
+      break;
+    case 'candlestick':
+      chartIcon = styles.candlestickChart;
+      break;
+    case 'boxplot':
+      chartIcon = styles.boxPlot;
+      break;
+    case 'radar':
+      chartIcon = styles.radarChart;
+      break;
+    case 'heatmap':
+      chartIcon = styles.heatmapChart;
+      break;
+    case 'datatable':
+      chartIcon = styles.dataTableChart;
+      break;
+    case 'map':
+      chartIcon = styles.mapChart;
+      break;
+    case 'sparkline':
+      chartIcon = styles.sparklineChart;
+      break;
+    default:
+      chartIcon = '';
+  }
 
   if (chartConfigHintTitle) {
     return (
@@ -25,6 +79,12 @@ const ChartConfigHintsView: React.FC<Props> = ({ chart, heightInRows, widthInCol
           )}
           {(heightInRows === undefined || heightInRows >= 2) && (
             <>
+              <div className={styles.chartType}>
+                {chartIcon && <div className={chartIcon} />}
+                <span className={styles.chartTypeName}>
+                  {`${chart.chartType[0].toUpperCase() + chart.chartType.slice(1)} Chart`}
+                </span>
+              </div>
               <div className={styles.uiHints}>{chartConfigHintTitle}</div>
               {chartConfigHintSubtitle ? <div className={styles.smallText}>{chartConfigHintSubtitle}</div> : undefined}
             </>
