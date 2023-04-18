7b8380290b5bc1848e08f635597b767fe75379ab
Petri Silen <petri.silen@nokia.com>
2023-04-18 12:14:41 +0300

    Changes

--- src/app/page/dataexplorer/chartareatabs/view/DataExplorerChartAreaTabsView.tsx
+++ src/app/page/dataexplorer/chartareatabs/view/DataExplorerChartAreaTabsView.tsx
@@ -1,10 +1,20 @@
 /* eslint-disable css-modules/no-undef-class */
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon, Menu, Popup, Tab } from 'semantic-ui-react';
 import styles from './DataExplorerChartAreaTabsView.module.scss';
 import ChartAreaView from '../../../../common/components/chartarea/view/ChartAreaView';
 
 const DataExplorerChartAreaTabsView = () => {
+  const [tabs, setTabs] = useState([] as string[]);
+
+  const addNewTab = () => {
+    setTabs([...tabs, `Sample tab ${tabs.length + 1}`]);
+  };
+
+  const removeTab = (tabToRemove: string) => {
+    setTabs(tabs.filter((tab) => tab !== tabToRemove));
+  };
+
   const panes = [
     {
       menuItem: (
@@ -37,18 +47,39 @@ const DataExplorerChartAreaTabsView = () => {
         </Menu.Item>
       ),
       render: () => <Tab.Pane />
-    },
-    {
-      menuItem: (
-        <Menu.Item key="Add new">
-          <Popup inverted mouseEnterDelay={1000} trigger={<Icon name="plus" />} content="Add new tab" />
-        </Menu.Item>
-      ),
-      render: () => <Tab.Pane />
     }
   ];
 
-  return <Tab activeIndex={0} className={styles.tab} panes={panes} />;
+  const additionalPanes = tabs.map((tab) => ({
+    menuItem: (
+      <Menu.Item key={tab}>
+        {tab}{' '}
+        <Popup
+          inverted
+          mouseEnterDelay={1000}
+          trigger={<Icon className={styles.closeIcon} name="close" onClick={() => removeTab(tab)} />}
+          content="Close tab"
+        />
+      </Menu.Item>
+    ),
+    render: () => <Tab.Pane />
+  }));
+
+  const addButtonTab = {
+    menuItem: (
+      <Menu.Item key="Add new">
+        <Popup
+          inverted
+          mouseEnterDelay={1000}
+          trigger={<Icon name="plus" onClick={addNewTab} />}
+          content="Add new tab"
+        />
+      </Menu.Item>
+    ),
+    render: () => <Tab.Pane />
+  };
+
+  return <Tab defaultActiveIndex={0} className={styles.tab} panes={[...panes, ...additionalPanes, addButtonTab]} />;
 };
 
 export default DataExplorerChartAreaTabsView;
