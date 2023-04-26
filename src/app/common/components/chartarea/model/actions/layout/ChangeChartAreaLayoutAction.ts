529d54f2640c3f7d30d8ae2f471980a123b9c387
Petri Silen <petri.silen@nokia.com>
2023-04-26 15:28:47 +0300

    Changes

--- src/app/common/components/chartarea/model/actions/layout/ChangeChartAreaLayoutAction.ts
+++ src/app/common/components/chartarea/model/actions/layout/ChangeChartAreaLayoutAction.ts
@@ -5,6 +5,7 @@ import type { Chart } from '../../../chart/model/state/Chart';
 import type { ChartAreaStateNamespace } from '../../state/types/ChartAreaStateNamespace';
 import ChartFactory from '../../../chart/model/state/ChartFactory';
 import { GridItem } from '../../state/types/GridItem';
+import scrollingLayout from '../../../../../../page/dataexplorer/pane/left/selector/layout/model/state/layouts/scrollingLayout';
 
 export default class ChangeChartAreaLayoutAction extends AbstractChartAreaAction {
   constructor(stateNamespace: ChartAreaStateNamespace, private readonly newLayout: GridItem[]) {
@@ -18,7 +19,8 @@ export default class ChangeChartAreaLayoutAction extends AbstractChartAreaAction
       return {
         ...currentState,
         layout: this.newLayout,
-        charts: _.take(charts, this.newLayout.length)
+        charts: _.take(charts, this.newLayout.length),
+        isScrollingLayout: this.newLayout === scrollingLayout
       };
     } else {
       const newCharts: Chart[] = [];
@@ -32,7 +34,8 @@ export default class ChangeChartAreaLayoutAction extends AbstractChartAreaAction
       return {
         ...currentState,
         layout: this.newLayout,
-        charts: [...charts, ...newCharts]
+        charts: [...charts, ...newCharts],
+        isScrollingLayout: this.newLayout === scrollingLayout
       };
     }
   }
