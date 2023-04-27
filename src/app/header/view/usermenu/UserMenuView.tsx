b0f7f719563128d85e09f75a337a36de2c99821b
Petri Silen <petri.silen@nokia.com>
2023-04-27 14:00:27 +0300

    Changes

--- src/app/header/view/usermenu/UserMenuView.tsx
+++ src/app/header/view/usermenu/UserMenuView.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import { Icon } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Dropdown, Icon } from 'semantic-ui-react';
 import styles from './UserMenuView.module.scss';
 
-const UserMenuView = () => (
-  <section className={styles.userMenu}>
-    <Icon name="user" />
-    <span>Petri</span>
-    <Icon name="dropdown" />
-  </section>
-);
+const UserMenuView = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  return (
+    <section className={styles.userMenu}>
+      <Icon name="user" />
+      <span>Petri</span>
+      <Dropdown open={menuIsOpen} direction="left" icon="dropdown" onClick={() => setMenuIsOpen(!menuIsOpen)}>
+        <Dropdown.Menu>
+          <Dropdown.Item value="Admin settings">
+            <div>
+              <Icon name="setting" />
+              <span>Admin settings</span>
+            </div>
+          </Dropdown.Item>
+          <Dropdown.Item value="User settings">
+            <div>
+              <Icon name="setting" />
+              <span>User settings</span>
+            </div>
+          </Dropdown.Item>
+          <Dropdown.Item value="Logout">
+            <div>
+              <Icon name="log out" />
+              <span>Log out</span>
+            </div>
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </section>
+  );
+};
 
 export default UserMenuView;
