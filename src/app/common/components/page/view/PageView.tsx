cfa20e6e30f1d456e51db3b0e8976c7f62da03ef
pksilen <petri.silen@silensoft.com>
2023-04-29 10:59:03 +0300

    Changes

--- src/app/common/components/page/view/PageView.tsx
+++ src/app/common/components/page/view/PageView.tsx
@@ -52,7 +52,7 @@ const PageView = ({
     if (event.pageX !== 0) {
       dragPagePaneGutter(pane, event.pageX);
     }
-  }, 50);
+  }, 200);
 
   const createDraggableGutter = (pane: Pane) => {
     if (shouldShowPagePanePermanently[pane]) {
