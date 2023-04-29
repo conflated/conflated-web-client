41cb59d56d0dba2f9ec50c12be9a1f98e30dfafa
pksilen <petri.silen@silensoft.com>
2023-04-29 18:58:37 +0300

    Changes

--- src/app/page/dataexplorer/pane/left/selector/measure/view/DraggableMeasureListItemView.tsx
+++ src/app/page/dataexplorer/pane/left/selector/measure/view/DraggableMeasureListItemView.tsx
@@ -4,7 +4,7 @@ import { Measure } from '../model/state/types/Measure';
 import MeasureListItemView from '../../../../../../../common/views/list/item/MeasureListItemView';
 
 const DraggableMeasureListItemView = ({ ...props }: ListItemViewProps<Measure>) => (
-  <MeasureListItemView draggable dragEventDataKey="dimensionName" {...props} />
+  <MeasureListItemView draggable dragEventDataKey="measureName" {...props} />
 );
 
 export default DraggableMeasureListItemView;
